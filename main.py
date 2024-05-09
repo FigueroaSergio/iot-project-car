6e0046e93685d5aff7435f994c3ae397e2597d03
Sergio Figueroa <figueroa.sergio.26@gmail.com>
2024-05-09 09:56:37 +0200

    FIX demo

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from models.sensor import Sensor
 
 
 def main():
-    s1= Sensor()
-    s2= Sensor()
-    s3=Sensor()
+    s1 = Sensor()
+    s2 = Sensor()
+    s3 = Sensor()
     sensors = SensorManager()
     sensors.subscribe(s1)
     sensors.subscribe(s2)
     sensors.subscribe(s3)
     car = Car(velocity=2, acceleration=10,sensors=sensors)
+
 
     print(car.model_dump())
 
