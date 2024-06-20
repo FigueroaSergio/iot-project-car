cac9e52a2e152b40441626a485b4c57990af4713
Sergio Figueroa <figueroa.sergio.26@gmail.com>
2024-06-20 09:35:38 +0200

    CHANGE to fast api to allow peer connection server

--- server/main.py
+++ server/main.py
@@ -1,17 +1,30 @@
+import eventlet
+# eventlet.monkey_patch()
 import cv2
 import numpy as np
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from fastapi import FastAPI, WebSocket
+import socketio
 from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
 from aiortc.contrib.media import MediaRelay
-import asyncio
+from fastapi.responses import HTMLResponse
+
+from fastapi.staticfiles import StaticFiles
+
+import logging
 
 from uuid import uuid4
+import uvicorn
 
-app = Flask(__name__)
-socketio = SocketIO(app,cors_allowed_origins="*")
+app = FastAPI(reload=True)
 
-pcs = {}
+sio =socketio.AsyncServer(async_mode='asgi',cors_allowed_origins='*')
+app.mount('/socket.io', socketio.ASGIApp(sio))
+app.mount("/static", StaticFiles(directory="./templates"), name="static")
+
+# Set the logging level (adjust as needed)
+
+
+pcs:set[RTCPeerConnection] = set()
 relay = MediaRelay()
 
 class VideoTransformTrack(VideoStreamTrack):
@@ -24,62 +37,157 @@ class VideoTransformTrack(VideoStreamTrack):
         
         # Process the frame with OpenCV
         img = frame.to_ndarray(format="bgr24")
+        # cv2.imshow("Server View", img)
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     pass
         height, width, _ = img.shape
         cv2.rectangle(img, (0, 0), (width, height), (0, 255, 0), 10)
-        
+        # cv2.imshow("Server View", img)
         # Convert back to video frame
         new_frame = frame.from_ndarray(img, format="bgr24")
         return new_frame
 
-@app.route('/')
+@app.get('/', response_class=HTMLResponse)
 def index():
-    return render_template('index.html')
-@app.route('/controller')
+    with open("templates/index.html") as f:
+        html_content = f.read()
+    return HTMLResponse(content=html_content)
+
+@app.get('/controller', response_class=HTMLResponse)
 def viewer():
-    return render_template('watcher.html')
+    with open("templates/watcher.html") as f:
+        html_content = f.read()
+    return HTMLResponse(content=html_content)
 
-@socketio.on('broadcaster')
-def broadcaster():
-    print('broadcaster')
-    emit('broadcaster',broadcast=True)
+@sio.on('broadcaster')
+async def broadcaster(id):
+    
+    print('broadcaster',id)
+    await sio.emit('broadcaster')
 
-@socketio.on('frame')
-def frame(data):
-    print('frame')
-    emit('frame',data,broadcast=True)
+# @sio.event
+# def frame(data):
+#     print('frame')
+#     sio.emit('frame',data,broadcast=True)
 
-@socketio.on('watcher')
-def watcher(id):
-    print('Received watcher event')
-    emit('watcher',id,broadcast=True)
+@sio.on('watcher')
+async def watcher(id):
+    print('Received watcher event', id)
+    await sio.emit('watcher', id)
     
 
-@socketio.on('offer')
-def on_offer(localDescription):
-    print('Received offer event')
-    emit('offer', localDescription, broadcast=True)
+@sio.on('offer')
+async def offer(id,localDescription):
+    print('Received offer event',id)
+    await sio.emit('offer', localDescription)
 
-@socketio.on('connect')
-def connect():
-    print('connection')
-    client_id = str(uuid4())  # Generar un ID único
-    emit('assign_id', {'id': client_id})
+# @sio.event
+# def connect():
+#     print('connection')
+#     client_id = str(uuid4())  # Generar un ID único
+#     emit('assign_id', {'id': client_id})
 
-@socketio.on('answer')
-def on_answer(localDescription):
-    print('Received answer event')
-    emit('answer', localDescription, broadcast=True)
+@sio.on('answer')
+async def answer(id,data):
+    print('Received answer event',id)
+    await sio.emit('answer', {'answer':data,'id':id})
 
-@socketio.on('candidate')
-def on_candidate(candidate):
+@sio.on('candidate')
+async def on_candidate(id,candidate):
     print('Received candidate event')
-    emit('candidate', candidate, broadcast=True)
+    await sio.emit('candidate', candidate)
 
-@socketio.on('disconnectPeer')
-async def on_disconnect():
-    pc = pcs.get()
-    await pc.close()
-    pcs.discard(pc)
+# @socketio.on('disconnectPeer')
+# async def on_disconnect():
+#     pc = pcs.get()
+#     await pc.close()
+#     pcs.discard(pc)
+
+
+@sio.on('stream')
+async def stream(id,localDescription):
+
+  
+    global pcs
+    print('Received stream')
+    offer = RTCSessionDescription(sdp=localDescription['sdp'], type=localDescription['type'])
+    pc = RTCPeerConnection()
+    pcs.add(pc)
+    @pc.on("icecandidate")
+    def on_icecandidate(event):
+        if event.candidate:
+            socketio.emit('candidate', {'candidate': event.candidate})
+    @pc.on("track")
+    def on_track(track):
+        print('track', track.kind)
+        if track.kind == "video":
+            pc.addTrack(VideoTransformTrack(track))
+    print("Setting remote description...")
+    await pc.setRemoteDescription(offer)
+    print("Remote description set.")
+    try:
+        print("Creating answer...")
+        answer = await pc.createAnswer()
+        if not answer:
+            print('No answer created')
+            return
+        print("Answer created.")
+        await pc.setLocalDescription(answer)
+    except Exception as e:
+        logging.error(f"Error processing data: {e}")
+
+    try:
+        print('answering')
+        await sio.emit('answer', {'answer':{'sdp': pc.localDescription.sdp, 'type': pc.localDescription.type},'id':'server'})
+    except Exception as e:
+        print(f"Error in stream function: {e}")
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000,debug=True)
+    uvicorn.run(app, host='0.0.0.0', port=5000)
+
+# @socketio.on('stream')
+# def stream(localDescription):
+#     app.logger.debug(f"Received data from client: {localDescription}")
+
+# async def stream(localDescrition):
+#     # print(localDescrition)
+#     app.logger.debug(f"Received data from client: {localDescrition}")
+#     # ... your processing code ...
+#     async def process_stream(localDescrition):
+#         global pcs
+#         print('Recived stream')  
+#         emit('answer','answer',broadcast=True)
+#         # print(localDescrition)  
+#         offer = RTCSessionDescription(sdp=localDescrition['sdp'], type=localDescrition['type'])
+#         pc = RTCPeerConnection()
+#         pcs.add(pc)
+
+#         @pc.on("icecandidate")
+#         def on_icecandidate(event):
+#             if event.candidate:
+#                 socketio.emit('candidate', {'candidate': event.candidate})
+
+#         @pc.on("track")
+#         def on_track(track):
+#             print('track',track.kind)
+#             if track.kind == "video":
+#                 pc.addTrack(VideoProcessorTrack(track))
+
+    
+#         await pc.setRemoteDescription(offer)
+#         answer = await pc.createAnswer()
+#         if(not answer):
+#             print('no answer')
+#             return
+#         await pc.setLocalDescription(answer)
+#         return answer
+   
+#     try:
+#         response = await process_stream(localDescrition)
+#         print('answering')
+#         emit('answer',response,broadcast=True)    
+#     except Exception as e:
+#         print(f"Error processing data: {e}")
+#         emit('my_error', {'message': 'An error occurred during processing.'})
+
+
