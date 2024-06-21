c92d985c1f11d25ac75f022718872b2652ad3b3d
Sergio Figueroa <figueroa.sergio.26@gmail.com>
2024-06-21 08:26:41 +0200

    ADD prove image processing

--- server/main.py
+++ server/main.py
@@ -11,9 +11,10 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 
 import logging
+import asyncio
 
-from uuid import uuid4
 import uvicorn
+from image import frame_processor
 
 app = FastAPI(reload=True)
 
@@ -33,16 +34,18 @@ class VideoTransformTrack(VideoStreamTrack):
         self.track = track
 
     async def recv(self):
+        print('video')
         frame = await self.track.recv()
-
         # Convert frame to OpenCV format
         img = frame.to_ndarray(format="bgr24")
-
         # Process frame (display it)
-        cv2.imshow("Server View", img)
+        r = frame_processor(img)
+        imS = cv2.resize(r, (360, 360))     
+        cv2.imshow("Server View", imS)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             pass
-
+        # Introduce a delay to control the frequency
+        # await asyncio.sleep(0.1)  # Adjust the delay time (in seconds) as needed
         # Return the frame unchanged
         return frame
 
